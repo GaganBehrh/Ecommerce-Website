89a44fff1b60faf6080d2a3de3addb5df36134f5
Gagan Behrh <gaganpreet.behrh3@gmail.com>
2022-03-24 16:01:30 +0100

    order button added

--- src/script.js
+++ src/script.js
@@ -7,6 +7,9 @@ function showNewsPage() {
 function showAboutPage() {
   window.location.assign("about.html");
 }
+function showOrderPage() {
+  window.location.assign("order.html");
+}
 let contact = document.getElementById("contact");
 contact.addEventListener("click", showContactPage);
 let news = document.getElementById("news");
@@ -14,3 +17,6 @@ news.addEventListener("click", showNewsPage);
 
 let about = document.getElementById("about");
 about.addEventListener("click", showAboutPage);
+
+let orderbutton = document.getElementById("orderbutton");
+orderbutton.addEventListener("click", showOrderPage);
